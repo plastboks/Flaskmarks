29044936dba880eebbbc12fa83456416f5c8ccec
Alexander Skjolden <alex@plastboks.net>
2013-05-21 19:50:51 +0200

    messing around

---
+++ deploy.sh
@@ -0,0 +1,10 @@
+pip install flask
+pip install flask-login
+pip install flask-openid
+pip install flask-mail
+pip install flask-sqlalchemy
+pip install sqlalchemy-migrate
+pip install flask-whooshalchemy
+pip install flask-wtf
+pip install flask-babel
+pip install flup
