1defb83445c22b67f4d855fda3fdaa5713600deb
Alexander Skjolden <alex@plastboks.net>
2013-05-22 15:51:46 +0200

    removed unneessary dependencies

--- deploy.sh
+++ deploy.sh
@@ -1,9 +1,7 @@
 pip install flask
 pip install flask-login
-pip install flask-mail
 pip install flask-sqlalchemy
 pip install sqlalchemy-migrate
 pip install flask-whooshalchemy
 pip install flask-wtf
-pip install flask-babel
 pip install flup
