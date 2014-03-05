a0aa9d6dd78d7e7fa2a696667ddeb7a2d315fef3
Alexander Skjolden <alex@plastboks.net>
2014-03-05 21:06:03 +0100

    added a tag model, not used for now
    Signed-off-by: Alexander Skjolden <alex@plastboks.net>

--- flaskmarks/models/__init__.py
+++ flaskmarks/models/__init__.py
@@ -2,3 +2,4 @@
 
 from flaskmarks.models.user import User
 from flaskmarks.models.mark import Mark
+from flaskmarks.models.tag import Tag
---
+++ flaskmarks/models/tag.py
@@ -0,0 +1,15 @@
+from flaskmarks import db, config
+from sqlalchemy import (
+    and_,
+    or_,
+    desc,
+)
+
+
+class Tag(db.Model):
+    __tablename__ = 'tags'
+    id = db.Column(db.Integer, primary_key=True)
+    title = db.Column(db.Unicode(255), nullable=False)
+
+    def __repr__(self):
+        return '<Tag %r>' % (self.title)
