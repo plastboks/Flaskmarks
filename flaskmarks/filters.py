4e793d48a984767b653680a6ed70dd2e232e7dbb
Alexander Skjolden <alex@plastboks.net>
2014-03-30 22:03:28 +0200

    changed gravatar implementation with much simpler one
    Signed-off-by: Alexander Skjolden <alex@plastboks.net>

--- flaskmarks/filters.py
+++ flaskmarks/filters.py
@@ -1,7 +1,7 @@
 from flaskmarks import app
 from datetime import datetime
-from flask.ext.gravatar import Gravatar
 from webhelpers2.date import time_ago_in_words
+from hashlib import md5
 
 
 @app.template_filter('date')
@@ -14,13 +14,7 @@ def _jinja2_filter_dateinwords(date):
     return time_ago_in_words(date, round=True, granularity='day')
 
 @app.template_filter('gravatar')
-def _jinja_filter_gravatar(size = False, rating = False):
-    gravatar = Gravatar(app,
-                     size=100,
-                     rating='r',
-                     default='retro',
-                     force_default=False,
-                     force_lower=False,
-                     use_ssl=True,
-                     base_url=None)
-    return gravatar
+def _jinja_filter_gravatar(email):
+    url = 'https://www.gravatar.com/avatar/'
+    params = '?d=mm&s='+str(100)
+    return  url + md5(email).hexdigest() + params
