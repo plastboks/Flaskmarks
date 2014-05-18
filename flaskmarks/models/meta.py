0a2321941257a49f4a651252b3f98f1ef11f842d
Alexander Skjolden <alex@plastboks.net>
2014-05-18 22:16:12 +0200

    bad column name for meta table
    Signed-off-by: Alexander Skjolden <alex@plastboks.net>

--- flaskmarks/models/meta.py
+++ flaskmarks/models/meta.py
@@ -10,7 +10,7 @@ class Meta(db.Model):
     __tablename__ = 'meta'
     id = db.Column(db.Integer, primary_key=True)
     mark_id = db.Column(db.Integer, db.ForeignKey('marks.id'))
-    key = db.Column(db.Unicode(255), nullable=False)
+    name = db.Column(db.Unicode(255), nullable=False)
     value = db.Column(db.Unicode(255), nullable=False)
 
     def __repr__(self):
