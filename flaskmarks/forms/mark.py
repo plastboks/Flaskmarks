685866670530a1a90b73bd8a57fd01f336b8197c
Alexander Skjolden <alex@plastboks.net>
2014-05-29 21:42:33 +0200

    helper class for mark forms, not properly implemented
    Signed-off-by: Alexander Skjolden <alex@plastboks.net>

--- flaskmarks/forms/mark.py
+++ flaskmarks/forms/mark.py
@@ -15,6 +15,10 @@ from wtforms import (
     IntegerField,
 )
 
+"""
+Code inspired from WTForms Documentation.
+http://wtforms.simplecodes.com/docs/1.0.2/fields.html#custom-fields
+"""
 class TagListField(Field):
     widget = TextField()
 
@@ -29,6 +33,27 @@ class TagListField(Field):
             self.data = [x.strip() for x in valuelist[0].split(',')]
         else:
             self.data = []
+
+
+class BetterTagListField(TagListField):
+    def __init__(self, label='', validators=None, remove_duplicates=True, **kwargs):
+        super(BetterTagListField, self).__init__(label, validators, **kwargs)
+        self.remove_duplicates = remove_duplicates
+
+    def process_formdata(self, valuelist):
+        super(BetterTagListField, self).process_formdata(valuelist)
+        if self.remove_duplicates:
+            self.data = list(self._remove_duplicates(self.data))
+
+    @classmethod
+    def _remove_duplicates(cls, seq):
+        """Remove duplicates in a case insensitive, but case preserving manner"""
+        d = {}
+        for item in seq:
+            if item.lower() not in d:
+                d[item.lower()] = True
+                yield item
+
 
 class MarkForm(Form):
     referrer = HiddenField([validators.URL(require_tld=False)])
